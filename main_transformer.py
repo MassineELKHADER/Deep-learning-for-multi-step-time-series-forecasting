66dff41dfa5c50f15f4d4ec68cd1711b141bbb61
MassineELKHADER <massineelkhader@gmail.com>
2025-12-13 14:57:51 +0100

    weights for Transformer and GRU with L2 Omega penalty

--- main_transformer.py
+++ main_transformer.py
@@ -31,8 +31,8 @@ N_input = 20
 N_output = 20  
 sigma = 0.01
 gamma = 0.01
-epochs = 5
-# epochs = 500
+# epochs = 5
+epochs = 500
 
 def train_model(net,loss_type, learning_rate, epochs=1000, gamma = 0.001,
                 print_every=50,eval_every=50, verbose=1, Lambda=1, alpha=0.5):
